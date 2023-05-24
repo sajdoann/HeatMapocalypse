3eddd3ca45820923e589554d3e5a05d182c30d01
sajdoann <sajdoann@fit.cvut.cz>
2023-05-24 23:10:33 +0800

    resturcturing

---
+++ world_map/world_map.js
@@ -0,0 +1,113 @@
+var width = window.innerWidth,
+    height = window.innerHeight;
+
+var projection = d3.geoNaturalEarth1()
+    .translate([width / 2, height / 2])
+    .scale(width / 6.5);
+
+var svg = d3.select("#map-container").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+var path = d3.geoPath()
+    .projection(projection);
+
+var g = svg.append("g");
+
+// Define the color scale
+var colorScale = d3.scaleLinear()
+    .domain([0, 30])
+    .range(["blue", "red"]);
+
+
+// load and display the World
+d3.json("../data/world-110m2.json")
+    .then(function (topology) {
+        var slider = d3.select("#mySlider");
+        var textbox = d3.select("#textbox");
+
+        // load and display the cities
+        d3.csv("../data/GlobalTemperatures1m.csv")
+            .then(function (data) {
+                var circles;
+
+                slider.on("input", function () {
+                    var value = +this.value;
+                    var year = Math.floor(value / 12) + 1743;
+                    var month = (value % 12) + 1;
+                    var monthString = ("0" + month).slice(-2);
+                    var selectedYearMonth = year + "-" + monthString;
+                    textbox.text("Year: " + year + ", Month: " + month).style("color", "black");
+
+                    // Filter the data based on the selected year and month
+                    var filteredData = data.filter(function (d) {
+                        var dataYear = parseInt(d.yearmonth.substr(0, 4));
+                        var dataMonth = parseInt(d.yearmonth.substr(5, 2));
+                        return dataYear === year && dataMonth === month;
+                    });
+
+                    // Update the data binding for the circles
+                    circles = g.selectAll("circle").data(filteredData);
+
+                    circles.enter()
+                        .append("circle")
+                        .attr("cx", function (d) {
+                            return projection([+d.lon, +d.lat])[0];
+                        })
+                        .attr("cy", function (d) {
+                            return projection([+d.lon, +d.lat])[1];
+                        })
+                        .attr("r", 5)
+                        .style("opacity", 1)
+                        .style("fill", function (d) {
+                            return colorScale(+d.AverageTemperature);
+                        });
+
+                    circles.exit().remove();
+                });
+
+                // Update the existing circles' positions
+                slider.on("change", function () {
+                    circles
+                        .attr("cx", function(d) {
+                            return projection([+d.lon, +d.lat])[0];
+                        })
+                        .attr("cy", function(d) {
+                            return projection([+d.lon, +d.lat])[1];
+                        })
+                        .style("fill", function(d) {
+                            return colorScale(+d.AverageTemperature);
+                        });
+                });
+
+                // Initialize with the default values
+                slider.dispatch("input");
+            });
+
+        g.selectAll("path")
+            .data(topojson.feature(topology, topology.objects.countries).features)
+            .join("path")
+            .attr("d", path)
+            .on("mouseover", function() {
+                // Show the country name on hover
+                d3.select(this).classed("hovered", true);
+
+                // Get the country name from the data
+                var countryName = d.properties.name;
+
+                // Append a text element for the country label
+                svg.append("text")
+                    .attr("class", "country-label")
+                    .attr("x", projection(d3.geo.centroid(d))[0])
+                    .attr("y", projection(d3.geo.centroid(d))[1])
+                    .text(countryName);
+
+            })
+            .on("mouseout", function() {
+                // Hide the country name on mouseout
+                d3.select(this).classed("hovered", false);
+                svg.select(".country-label").remove();
+
+            });
+
+    });
